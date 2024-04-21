b5139642165bb748f769490e92f2f233dbdf8902
Luda <ludmila.mizus@gmail.com>
2024-04-21 12:00:59 +0300

    Button "Generate New" now functioning.

--- seeds.py
+++ seeds.py
@@ -3,24 +3,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/get_seed')
+def get_seed():
+    seed = random.randint(-100000000000000000000000, 100000000000000000000000)
+    return f'{{ "seed": "{str(seed)}" }}'
+
 @app.route('/')
 def seedGen():
-    seed = random.randint(-100000000000000000000000, 100000000000000000000000)
+    # request_seed() is a function that requests a seed on page's load and while clicking on a "Generate New" button.
+    # Line 18: Requests new seed while entering /get_seed URL, then on line 19 on load of the page function()
+    # create an object which parses responseText. Then it creates a new seed on the <span> text line on line 34 after requesting text line with the ID "demo"
+    script = '''
+function request_seed() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    const obj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = obj.seed;
+  }
+  xhttp.open("GET", "/get_seed", true);
+  xhttp.send();
+}
+'''
     return f'''
 <html>
-<body>
+<body onload="request_seed();">
 
 <h1>MC Seed Generator</h1>
 
 <p>Select and press Ctrl+C to Copy (Ctrl+V to Paste).</p>
-<form>
-<p>Seed: {str(seed)}</p>
-</form>
-<button class="button" type="button" onclick="">Generate New</button>
-
+<p>Seed: <span id="demo">N/A</span></p>
+<button class="button" type="button" onclick="request_seed();">Generate New</button>
 <p>Good Luck Finding the Best Seed :)</p>
 
 </body>
+<script>
+  {script}
+</script>
 </html>
 '''
 
